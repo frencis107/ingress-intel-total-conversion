6ffb371c05638c2df4774f8bae2ce09c5b9cfb57
johndoe <johnd0e@mail.ua>
2021-04-22 14:51:17 +0300

    map.js: put all location-related in one place

--- core/code/map.js
+++ core/code/map.js
@@ -233,14 +233,6 @@ window.setupMap = function() {
 
   map.attributionControl.setPrefix('');
 
-  map.on('moveend', function () {
-    var center = this.getCenter().wrap();
-    window.writeCookie('ingress.intelmap.lat', center.lat);
-    window.writeCookie('ingress.intelmap.lng', center.lng);
-    window.writeCookie('ingress.intelmap.zoom', this.getZoom());
-  });
-
-
   // map update status handling & update map hooks
   // ensures order of calls
   map.on('movestart', function() { window.mapRunsUserAction = true; window.requests.abort(); window.startRefreshTimeout(-1); });
@@ -274,6 +266,22 @@ window.setupMap = function() {
       map.locate({setView: true});
     }
     map.setView(pos.center, pos.zoom, {reset: true});
+
+    // read here ONCE, so the URL is only evaluated one time after the
+    // necessary data has been loaded.
+    var pll = window.getURLParam('pll');
+    if (pll) {
+      pll = pll.split(',');
+      window.urlPortalLL = normLL(pll[0], pll[1]).center;
+    }
+    window.urlPortal = window.getURLParam('pguid');
+
+    map.on('moveend', function () {
+      var center = this.getCenter().wrap();
+      window.writeCookie('ingress.intelmap.lat', center.lat);
+      window.writeCookie('ingress.intelmap.lng', center.lng);
+      window.writeCookie('ingress.intelmap.zoom', this.getZoom());
+    });
 
     // todo check
     // leaflet no longer ensures the base layer zoom is suitable for the map (a bug? feature change?), so do so here
